9220e2debbd1c7f8cf0aec8ad82d4799e91d8808
UpadhyayAshmita <au08019@uga.edu>
2025-11-14 07:43:31 -0500

    added modififed code

--- script/pteranodontest.sh
+++ script/pteranodontest.sh
@@ -5,8 +5,8 @@
 #SBATCH -N 1
 #SBATCH -p khufu
 #SBATCH --mem=128G
-#SBATCH -o "stds/stdout_%x_%J"
-#SBATCH -e "stds/stderr_%x_%J"
+#SBATCH -o "/cluster/lab/clevenger/Ashmita/assembly/log/pteranodon_%x_%J.out"
+#SBATCH -e "/cluster/lab/clevenger/Ashmita/assembly/log/pteranodon_%x_%J.err"
 
 # Path to Pteranodon installation
 pteranodon="/cluster/home/wkorani/korani_aps/pteranodon"
@@ -15,18 +15,17 @@ t=8
 # Reference genome (Tifrunner V2)
 ref="/cluster/lab/clevenger/Ashmita/assembly/ref/tifrunner_v2.fa"
 
-# One of your query contigs (50k filtered)
-query="/cluster/lab/clevenger/Ashmita/assembly/TifTB.asm.bp.p_ctg.50k.fa"
+# One of the 50k filtered contigs
+query="/cluster/lab/clevenger/Ashmita/assembly/AU-23-10_AU-23-10.asm.bp.p_ctg.50k.fa.gz"
 
-# Output folder name
-out="TifTB_TR2_50k_scaffold "
+# Output folder
+out="/cluster/lab/clevenger/Ashmita/assembly/AU-23-10_scaffolded"
 
-# Minimum contig length threshold (1000 bp)
+# Minimum contig length threshold
 len=1000
 
 # Minimum percent identity
 min=1
 
 # Run Pteranodon
-
 "$pteranodon"/get_plot_based_on_local_alignment.sh "$pteranodon" "$t" "$ref" "$query" "$out" "$len" "$min"
