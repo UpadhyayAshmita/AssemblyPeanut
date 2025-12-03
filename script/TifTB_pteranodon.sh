f4231101471fc1a4620c8162ea664a4f1938b3ca
UpadhyayAshmita <au08019@uga.edu>
2025-12-03 10:40:05 -0500

    TifTB

--- script/TifTB_pteranodon.sh
+++ script/TifTB_pteranodon.sh
@@ -13,7 +13,7 @@ source /cluster/projects/khufu/korani_projects/KhufuEnv/KhufuEnv.sh
 source /cluster/projects/khufu/korani_projects/load_modules.sh
 
 INPUT_DIR="/cluster/lab/clevenger/Ashmita/assembly"
-ref="${INPUT_DIR}/ref/tifrunner_v2.fa"
+ref="${INPUT_DIR}/ref/tifrunner_v2_filt.fa"
 
 query="${INPUT_DIR}/TifTB.asm.bp.p_ctg.50k.fa"
 basename=$(basename "$query")
