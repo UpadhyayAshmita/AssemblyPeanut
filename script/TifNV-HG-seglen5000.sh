80b30ef609b8d4f55bebaff0dfbd9a5510a54289
UpadhyayAshmita <au08019@uga.edu>
2025-12-15 15:25:22 -0500

    added

--- script/TifNV-HG-seglen5000.sh
+++ script/TifNV-HG-seglen5000.sh
@@ -12,32 +12,44 @@
 source /cluster/projects/khufu/korani_projects/KhufuEnv/KhufuEnv.sh
 source /cluster/projects/khufu/korani_projects/load_modules.sh
 
-INPUT_DIR="/cluster/lab/clevenger/Ashmita/assembly"
-ref="${INPUT_DIR}/ref/tifrunner_v2_filt.fa"
+BASE_DIR="/cluster/lab/clevenger/Ashmita/assembly"
 
-query="${INPUT_DIR}/TifNV-HG.asm.bp.p_ctg.25k.fa"
+# Reference
+ref="${BASE_DIR}/ref/tifrunner_v2_filt.fa"
+
+# Input assembly 
+query="${BASE_DIR}/filt_assembly/TifNV-HG.asm.bp.p_ctg.25k.fa"
 basename=$(basename "$query")
 sample=${basename%.asm.bp.p_ctg.25k.fa}
 
-# Output directory
-out="${INPUT_DIR}/${sample}_scaffold_25k_min1_5000"
-mkdir -p "$out"
+# Output directories
+OUT_FA="${BASE_DIR}/scaffold_25k"
+OUT_RDS="${BASE_DIR}/rds_file"
+OUT_PDF="${BASE_DIR}/pdf"
+
+mkdir -p "$OUT_FA" "$OUT_RDS" "$OUT_PDF"
 
 echo "Running Pteranodon for sample: $sample"
 echo "Query:  $query"
-echo "Output: $out"
 
 # Parameters
 SegLen=5000
 MinQueryLen=1
 threads=24
 
+
+OUT_NAME="${sample}_SegLen${SegLen}_minQ${MinQueryLen}"
+
 # Run Pteranodon
 /cluster/projects/khufu/korani_projects/Pteranodon/scripts/PteranodonBase.sh \
     -ref "$ref" \
     -query "$query" \
-    -o "$out" \
+    -o "${OUT_FA}/${OUT_NAME}" \
     -SegLen $SegLen \
     -MinQueryLen $MinQueryLen \
     -auto 1 \
     -t $threads
+
+# Move auxiliary outputs 
+mv "${OUT_FA}/${OUT_NAME}"/*.rds "$OUT_RDS"/
+mv "${OUT_FA}/${OUT_NAME}"/*.pdf "$OUT_PDF"/
