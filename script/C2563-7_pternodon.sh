5d388e6f4b35490c490f5caa037e354c6ec0b3ec
UpadhyayAshmita <au08019@uga.edu>
2025-12-30 11:25:05 -0500

    added

--- script/C2563-7_pternodon.sh
+++ script/C2563-7_pternodon.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-#SBATCH -J ptero_C2563-7
+#SBATCH -J ptero_C2563-7_autofix
 #SBATCH --time=48:00:00
 #SBATCH -c 24
 #SBATCH -N 1
 #SBATCH -p khufu
 #SBATCH --mem=180G
-#SBATCH -o "/cluster/lab/clevenger/Ashmita/assembly/log/ptero_%x_%j.out"
-#SBATCH -e "/cluster/lab/clevenger/Ashmita/assembly/log/ptero_%x_%j.err"
+#SBATCH -o "/cluster/lab/clevenger/Ashmita/assembly/log/ptero_autofix_%x_%j.out"
+#SBATCH -e "/cluster/lab/clevenger/Ashmita/assembly/log/ptero_autofix_%x_%j.err"
 
 # Load Khufu + Pteranodon env
 source /cluster/projects/khufu/korani_projects/KhufuEnv/KhufuEnv.sh
 source /cluster/projects/khufu/korani_projects/load_modules.sh
 
-INPUT_DIR="/cluster/lab/clevenger/Ashmita/assembly"
+INPUT_DIR="/cluster/lab/clevenger/Ashmita/assembly/unfilt_assembly"
 ref="${INPUT_DIR}/ref/tifrunner_v2_filt.fa"
 
-query="${INPUT_DIR}/C2563-7.asm.bp.p_ctg.20k.fa"
+query="${INPUT_DIR}/C2563-7.asm.bp.p_ctg.fa"
 basename=$(basename "$query")
-sample=${basename%.asm.bp.p_ctg.20k.fa}
+sample=${basename%.asm.bp.p_ctg.fa}
 
 # Output directory
-out="${INPUT_DIR}/${sample}_scaffold_20k_min0.05"
+out="${INPUT_DIR}/${sample}_scaffold_autofix_min.01"
 mkdir -p "$out"
 
 echo "Running Pteranodon for sample: $sample"
@@ -28,9 +28,9 @@ echo "Query:  $query"
 echo "Output: $out"
 
 # Parameters
-SegLen=5000
-MinQueryLen=0.05
-threads=24
+SegLen=1000
+MinQueryLen=.01
+threads=32
 
 # Run Pteranodon
 /cluster/projects/khufu/korani_projects/Pteranodon/scripts/PteranodonBase.sh \
@@ -41,3 +41,20 @@ threads=24
     -MinQueryLen $MinQueryLen \
     -auto 1 \
     -t $threads
+
+
+
+awk '/^>/ {
+        if (seq) print seq;
+        print;
+        seq="";
+        next
+     }
+     {
+        seq = seq $0
+     }
+     END {
+        print seq
+     }' \
+  "${out}.fa" \
+> "${out}.joined.fa"
